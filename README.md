20805eea1b9f5d551cf3a3bf7d251f983b64f3a3
Catalina Restrepo Salgado <rposalgadoc@gmail.com>
2023-11-26 21:58:52 -0500

    Trying to add styles

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# VidoShop-BD
+# Vido Shop - Bases de Datos I
+
+## Elaborado por
+* Carolina Álvarez Murillo
+* Miller Johan Chica Acero
+* Catalina Restrepo Salgado
+
+Universidad Nacional de Colombia Sede Medellín
+
+Facultad de Minas
+
+Noviembre 27 de 2023
--- index.php
+++ index.php
@@ -6,9 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trabajo final BD1</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" 
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css" type="text/css">
 </head>
 
 <body>
